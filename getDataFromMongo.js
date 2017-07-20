4d3a17fd848347530bf536bfa3c83bcf05353cba
tangweikun <819573105@qq.com>
2017-07-20 21:32:35 +0800

    :wrench: add koa-body and koa-router

--- getDataFromMongo.js
+++ getDataFromMongo.js
@@ -6,7 +6,7 @@ export async function getDataFromMongo() {
   let result = []
 
   await MongoClient.connect(process.env.MONGO_URL).then(async db => {
-    result = await db.collection('users').find().toArray()
+    result = await db.collection('soap').find().toArray()
     console.log('--->', result)
     db.close()
   }).catch(error => console.error(error))
--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 import {getDataFromMongo} from './getDataFromMongo'
 import Koa from 'koa'
+import Router from 'koa-router'
+import koaBody from 'koa-body'
 
 const app = new Koa()
+const router = new Router()
+app.use(koaBody())
 
-app.use( async ( ctx ) => {
-  ctx.body = 'hello koa2'
-  await getDataFromMongo()
-})
+router
+  .get('/',function (ctx, next) {
+    getDataFromMongo().then(result => console.log('=====', result.length))
+    ctx.body = 'Hello World!';
+  })
+  .get('/t', function (ctx, next) {
+    ctx.body = 'twk'
+  })
+  .post('/r', function (ctx) {
+    console.log('----', ctx, ctx.request.body)
+    ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`
+    console.log('=======')
+  })
+
+app
+  .use(router.routes())
+  // .use(router.allowedMethods());
 
 app.listen(3000)
 console.log('[demo] start-quick is starting at port 3000')
