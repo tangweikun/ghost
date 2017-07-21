c3002c5ecfdc2b531ce04d081843ddfa9a333377
tangweikun <819573105@qq.com>
2017-07-21 11:22:31 +0800

    use mongoose instead of mongo

--- index.js
+++ index.js
@@ -2,14 +2,32 @@ import {getDataFromMongo} from './getDataFromMongo'
 import Koa from 'koa'
 import Router from 'koa-router'
 import koaBody from 'koa-body'
+import mongoose from 'mongoose'
+require('dotenv').config()
+mongoose.connect(process.env.MONGO_URL)
+// var Schema = mongoose.Schema
 
+// var SOAPSchema = new Schema({ author: String })
+var Schema = mongoose.Schema;
+
+var BearSchema = new Schema({
+});
+var SOAPModel = mongoose.model('user', BearSchema)
 const app = new Koa()
 const router = new Router()
 app.use(koaBody())
 
 router
   .get('/',function (ctx, next) {
-    getDataFromMongo().then(result => console.log('=====', result.length))
+    var a = SOAPModel.findOne({})
+    a.select('username')
+    a.exec(function(err, per) {
+      console.log(per.username, '===', per, per.username)
+      var d=per.toObject()
+      console.log('======dd', d, d.username)
+    })
+    // console.log(a, '---')
+    // getDataFromMongo().then(result => console.log('=====', result.length))
     ctx.body = 'Hello World!';
   })
   .get('/t', function (ctx, next) {
