f953bb61fb38e32d7076c2ca83e417acedba3727
tangweikun <819573105@qq.com>
2017-07-22 20:00:42 +0800

    :rotating_light:

--- getDataFromMongo.js
+++ getDataFromMongo.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require('dotenv').config()
 
-import { MongoClient } from 'mongodb';
+import { MongoClient } from 'mongodb'
 
 // test for conect mongo
 export async function getDataFromMongo() {
-  let result = [];
+  let result = []
 
   await MongoClient.connect(process.env.MONGO_URL).then(async (db) => {
-    result = await db.collection('soap').find().toArray();
-    console.log('--->', result);
-    db.close();
-  }).catch(error => console.error(error));
+    result = await db.collection('soap').find().toArray()
+    console.log('--->', result)
+    db.close()
+  }).catch(error => console.error(error))
 
-  return result;
+  return result
 }
