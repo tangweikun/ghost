c4acf1ec94a35c863e0c3bd91273f8da8ea61cdc
tangweikun <819573105@qq.com>
2017-07-29 13:09:04 +0800

    :fire:

--- routes.js
+++ routes.js
@@ -5,8 +5,8 @@ import { findProperty, insertProperty, insertTask } from './controllers'
 const router = new Router()
 
 router
-  .get('/findProperty', findProperty)
-  .post('/insertProperty', insertProperty)
-  .post('/insertTask', insertTask)
+  .get('/property', findProperty)
+  .post('/property', insertProperty)
+  .post('/task', insertTask)
 
 export default router
