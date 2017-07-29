d61e81aeed0260285e065d7080f1d09bc0a43e83
tangweikun <819573105@qq.com>
2017-07-30 00:46:53 +0800

    play with socket.io

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import Koa from 'koa'
 import koaBody from 'koa-body'
 import mongoose from 'mongoose'
 import cors from 'koa-cors'
+// import io from 'socket.io'
 
 import router from './routes'
 
 require('dotenv').config()
+
+const http = require('http')
 
 mongoose.connect(process.env.MONGO_URL)
 
@@ -14,7 +17,53 @@ const app = new Koa()
 app.use(koaBody())
 app.use(cors())
 app.use(router.routes())
-// .use(router.allowedMethods())
+// app('/', (req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*')
+//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+//   next()
+// })
 
-app.listen(4000)
+// .use(router.allowedMethods())
+const server = http.createServer(app.callback())
+const io = require('socket.io')(server, {
+  origins: 'http://localhost:* http://127.0.0.1:*',
+  path: '/stomp',
+})
+
+io.set('transports', ['websocket'])
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log(msg, '---->>>')
+    socket.emit('chat message', msg)
+  })
+  socket.emit('ww', 'rrr')
+  socket.on('he', msg => console.log(msg, '==='))
+})
+
+server.listen(4000)
+
+// http.listen(4000)
 console.log('[demo] start-quick is starting at port 4000')
+
+// var app = require('express')()
+// var http = require('http').Server(app)
+//
+// var allowedOrigins = 'http://localhost:* http://127.0.0.1:*'
+// var path = '/stomp' // you need this if you want to connect to something other than the default socket.io path
+// var io = require('socket.io')(http, { origins: allowedOrigins, path })
+// var port = process.env.PORT || 4000
+//
+// app.get('/', function(req, res) {
+//   res.sendFile(__dirname + '/index.html')
+// })
+//
+// io.on('connection', function(socket) {
+//   socket.on('chat message', function(msg) {
+//     io.emit('chat message', msg)
+//   })
+// })
+//
+// http.listen(port, function() {
+//   console.log('listening on *:' + port)
+// })
