8180caf103dbbec4081d7b5575cc0ca74c34f2df
tangweikun <819573105@qq.com>
2017-07-30 10:41:11 +0800

    play with socket

--- index.js
+++ index.js
@@ -18,22 +18,15 @@ const app = new Koa()
 app.use(koaBody())
 app.use(cors())
 app.use(router.routes())
-// app('/', (req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-//   next()
-// })
 
-// .use(router.allowedMethods())
 const server = http.createServer(app.callback())
 const io = require('socket.io')(server, {
   origins: 'http://localhost:* http://127.0.0.1:*',
   path: '/stomp',
 })
 
-io.set('transports', ['websocket'])
-
 io.on('connection', (socket) => {
+  console.log('-------->>>>>>>>>>>>>>>')
   socket.on('chat message', async (msg) => {
     await TodoListModel.find()
       .sort({ createdAt: -1 })
@@ -54,27 +47,4 @@ io.on('connection', (socket) => {
 
 server.listen(4000)
 
-// http.listen(4000)
 console.log('[demo] start-quick is starting at port 4000')
-
-// var app = require('express')()
-// var http = require('http').Server(app)
-//
-// var allowedOrigins = 'http://localhost:* http://127.0.0.1:*'
-// var path = '/stomp' // you need this if you want to connect to something other than the default socket.io path
-// var io = require('socket.io')(http, { origins: allowedOrigins, path })
-// var port = process.env.PORT || 4000
-//
-// app.get('/', function(req, res) {
-//   res.sendFile(__dirname + '/index.html')
-// })
-//
-// io.on('connection', function(socket) {
-//   socket.on('chat message', function(msg) {
-//     io.emit('chat message', msg)
-//   })
-// })
-//
-// http.listen(port, function() {
-//   console.log('listening on *:' + port)
-// })
