a741e35c7816bc853d3605dc2bc1231cc1fa5ec7
tangweikun <819573105@qq.com>
2018-06-12 21:54:42 +0800

    :shit: Make new api

--- index.js
+++ index.js
@@ -1,20 +1,50 @@
-import Koa from 'koa'
-import koaBody from 'koa-body'
-import mongoose from 'mongoose'
-import cors from 'koa-cors'
-import router from './routes'
+// 使用 `eventproxy` 来控制并发
 
-require('dotenv').config()
+var eventproxy = require('eventproxy')
+var superagent = require('superagent')
+var cheerio = require('cheerio')
+var url = require('url')
+var express = require('express')
+var ep = new eventproxy()
 
-// mongoose.connect(process.env.MONGO_URL)
-mongoose.connect('mongodb://twk:twk@ds115738.mlab.com:15738/twk')
+var app = express()
+app.listen(4000, function() {
+  console.log('app is listening at port 4000')
+})
 
-const app = new Koa()
+app.get('/', function(request, response) {
+  const baseUrl = 'https://movie.douban.com/trailers'
+  const detailUrls = []
+  const list = []
 
-app.use(koaBody())
-app.use(cors())
-app.use(router.routes())
-// .use(router.allowedMethods())
+  superagent.get(baseUrl).end(function(err, res) {
+    if (err) return console.log(err)
+    const $ = cheerio.load(res.text)
+    $('#upcoming .list-items')
+      .find('.title')
+      .each(function(_, element) {
+        const href = $(element)
+          .children()
+          .attr('href')
+        detailUrls.push(href)
+      })
 
-app.listen(4000)
-console.log('[demo] start-quick is starting at port 4000')
+    detailUrls.forEach(function(x) {
+      superagent.get(x).end(function(err, res2) {
+        if (err) {
+          ep.emit('got_detail_html', [])
+        } else {
+          const $ = cheerio.load(res2.text)
+          ep.emit('got_detail_html', {
+            source: $('source').attr('src'),
+            title: $('h1 a').text(),
+          })
+        }
+      })
+    })
+
+    ep.after('got_detail_html', detailUrls.length, function(movies) {
+      response.send(movies)
+    })
+  })
+})
